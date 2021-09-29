b371c2ecfa93376dc4e00bbdd9ed36b00e82f65a
Christophe Chichmanian <chris.chich@hotmail.fr>
2021-09-29 13:57:37 +0200

    Moved db credentials to .env

--- microservices/ms-ingredient/db/sql_server/index.js
+++ microservices/ms-ingredient/db/sql_server/index.js
@@ -1,18 +1,22 @@
-const { Sequelize } = require("sequelize");
+const {
+  Sequelize
+} = require("sequelize");
 
-const sequelize = new Sequelize("killerbee", "sa", process.env.MSSQL_PASSWORD, {
-  host: "192.168.1.62",
+const sequelize = new Sequelize(process.env.MSSQL_DB, process.env.MSSQL_USERNAME, process.env.MSSQL_PASSWORD, {
+  host: process.env.MSSQL_HOST,
   dialect: "mssql",
   define: {
     timestamps: false,
   },
-  retry: { max: Infinity },
+  retry: {
+    max: Infinity
+  },
 });
 try {
   (async () => {
     await sequelize.authenticate();
+    console.log("Connection to SQL Server has been established successfully.");
   })();
-  console.log("Connection to SQL Server has been established successfully.");
 } catch (error) {
   console.log("Connection to SQL Server failed: ");
 }
